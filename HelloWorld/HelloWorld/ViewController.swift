3a08ade0cf19b2807bfd208d739d0cefe8ec04e7
seongbiny <s30ngb1n@gmail.com>
2021-10-02 15:55:04 +0900

    211002

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Seongbin Yun on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+
---
+++ ImageView/ImageView/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by Seongbin Yun on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+    }
+
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) { // true
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {  // false
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+}
+
